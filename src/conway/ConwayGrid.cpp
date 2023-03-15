bff4ce222f59a87df6aed6bc83b9bee41a640bd1
Chris Jenkins <cdpjenkins@gmail.com>
2023-03-15 18:12:59 +0000

    Add basic implementation of Conway's Game of Life
    Lifted from sdl-conway repo.

---
+++ src/conway/ConwayGrid.cpp
@@ -0,0 +1,87 @@
+#include <cstdio>
+#include <cstring>
+
+#include "ConwayGrid.hpp"
+
+ConwayGrid::ConwayGrid() {
+    init_to_blank();
+}
+
+void ConwayGrid::init_to_blank() {
+    current_grid = grid1;
+    next_grid = grid2;
+    width = GRID_WIDTH;
+    height = GRID_HEIGHT;
+    running = false;
+
+    memset(this->grid1, 0, sizeof(grid1));
+    memset(this->grid2, 0, sizeof(grid2));
+}
+
+void ConwayGrid::print() {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            if (cell_alive_at(x, y)) {
+                printf("x");
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int ConwayGrid::num_living_neighbours(int x, int y) {
+    int a1 = cell_alive_at(x-1, y-1) ? 1 : 0;
+    int a2 = cell_alive_at(x, y-1) ? 1 : 0;
+    int a3 = cell_alive_at(x+1, y-1) ? 1 : 0;
+    int a4 = cell_alive_at(x-1, y) ? 1 : 0;
+    int a5 = cell_alive_at(x+1, y) ? 1 : 0;
+    int a6 = cell_alive_at(x-1, y+1) ? 1 : 0;
+    int a7 = cell_alive_at(x, y+1) ? 1 : 0;
+    int a8 = cell_alive_at(x+1, y+1) ? 1 : 0;
+
+    return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8;
+}
+
+void ConwayGrid::step() {
+    if (running) {
+        for (int x = 1; x < width - 1; x++) {
+            for (int y = 1; y < height - 1; y++) {
+                bool new_value;
+                int live_neighbours;
+                
+                live_neighbours = num_living_neighbours(x, y);
+                if (cell_alive_at(x, y)) {
+                    new_value = live_neighbours == 2 || live_neighbours == 3;
+                } else {
+                    new_value = live_neighbours == 3;
+                }
+
+                next_grid[grid_index(x, y)] = new_value;
+            }
+        }
+
+        bool *temp_grid_ptr = next_grid;
+        next_grid = current_grid;
+        current_grid = temp_grid_ptr;
+
+        generation++;
+    }
+}
+
+void ConwayGrid::run() {
+    running = true;
+}
+
+void ConwayGrid::pause() {
+    running = false;
+}
+
+void ConwayGrid::invert_cell(int grid_x, int grid_y) {
+    if (!running) {
+        bool cell_value = current_grid[grid_index(grid_x, grid_y)];
+        current_grid[grid_index(grid_x, grid_y)] = !cell_value;
+    }
+}
---
+++ src/conway/ConwayGrid.hpp
@@ -0,0 +1,48 @@
+
+#ifndef CONWAY_GRID_H
+#define CONWAY_GRID_H
+
+#define GRID_WIDTH 320
+#define GRID_HEIGHT 160
+
+#define MAX_LINE_LENGTH 1000
+
+class ConwayGrid {
+public:
+    int width = GRID_WIDTH;
+    int height = GRID_HEIGHT;
+    bool running;
+    int generation = 0;
+
+    ConwayGrid();
+    void init_to_blank();
+
+    void print();
+    void step();
+    void run();
+    void pause();
+
+    void invert_cell(int grid_x, int grid_y);
+
+    inline bool cell_alive_at(int x, int y) {
+        return current_grid[grid_index(x, y)];
+    }
+
+private:
+    bool grid1[GRID_WIDTH * GRID_HEIGHT];
+    bool grid2[GRID_WIDTH * GRID_HEIGHT];
+    bool* current_grid;
+    bool* next_grid;
+
+    int num_living_neighbours(int x, int y);
+
+    inline void set_cell(bool* grid, int x, int y, bool value) {
+        current_grid[grid_index(x, y)] = value;
+    }
+
+    inline int grid_index(int column, int row) {
+        return row * width + column;
+    }
+};
+
+#endif // CONWAY_GRID_H
