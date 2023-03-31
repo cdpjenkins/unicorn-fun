fd7abcb5995996ec9fb526ee8b65e26de066b899
Chris Jenkins <cdpjenkins@gmail.com>
2023-03-31 21:25:34 +0100

    Split set_text, update and draw

--- src/cosmic/ScrollTextApp.cpp
+++ src/cosmic/ScrollTextApp.cpp
@@ -15,22 +15,25 @@ void ScrollTextApp::display_text(const char *text,
                                  pimoroni::CosmicUnicorn &cosmic_unicorn,
                                  pimoroni::PicoGraphics_PenRGB888 &graphics)
 {
-    printf("displacement: %d\n", scroll_displacement);
+    set_text(text);
+    update(graphics);
+    draw(cosmic_unicorn, graphics);
+}
 
-    constexpr double TEXT_SCALE = 0.55;
-
+void ScrollTextApp::draw(pimoroni::CosmicUnicorn &cosmic_unicorn, pimoroni::PicoGraphics_PenRGB888 &graphics) const {
     graphics.set_pen(0, 0, 0);
     graphics.clear();
-
-    int32_t line_width = graphics.measure_text(text, TEXT_SCALE);
-    printf("line_width: %d\n", line_width);
-
     graphics.set_pen(32, 32, 32);
-    graphics.text(text, pimoroni::Point(scroll_displacement--, 5), -1, TEXT_SCALE);
-
+    graphics.text(this->text, pimoroni::Point(scroll_displacement, 5), -1, TEXT_SCALE);
     cosmic_unicorn.update(&graphics);
+}
 
+void ScrollTextApp::update(pimoroni::PicoGraphics_PenRGB888 &graphics) {
+    int32_t line_width = graphics.measure_text(this->text, TEXT_SCALE);
+    scroll_displacement--;
     if ((-scroll_displacement) > line_width) {
         scroll_displacement = pimoroni::CosmicUnicorn::WIDTH;
     }
 }
+
+void ScrollTextApp::set_text(const char *text) { ScrollTextApp::text = text; }
--- src/cosmic/ScrollTextApp.hpp
+++ src/cosmic/ScrollTextApp.hpp
@@ -1,6 +1,7 @@
 #ifndef UNICORN_FUN_SCROLLTEXTAPP_HPP
 #define UNICORN_FUN_SCROLLTEXTAPP_HPP
 
+#include <string>
 
 #include <cosmic_unicorn.hpp>
 
@@ -10,7 +11,17 @@ public:
     void display_text(const char *text, pimoroni::CosmicUnicorn &cosmic_unicorn, pimoroni::PicoGraphics_PenRGB888 &graphics);
 
 private:
+    std::string text;
     int scroll_displacement;
+
+    void set_text(const char *text);
+
+    static constexpr double TEXT_SCALE = 0.55;
+
+    void update(pimoroni::PicoGraphics_PenRGB888 &graphics);
+
+    void
+    draw(pimoroni::CosmicUnicorn &cosmic_unicorn, pimoroni::PicoGraphics_PenRGB888 &graphics) const;
 };
 
 
